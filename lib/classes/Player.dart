3a603ee116afe81168516821d6a003557e6803f5
David-S-2002 <dstanko@seattleu.edu>
2020-09-26 15:06:23 -0700

    Update Player.dart
    Made a function that converts D:H:M string to an int that represents minutes

--- lib/classes/Player.dart
+++ lib/classes/Player.dart
@@ -4,7 +4,7 @@
 //Minutes: 0-59
 class Player {
   String name;
-  double skill;
+  int skill;
 
   Player(String playerName, String dhm) {
     //constructor
@@ -18,8 +18,18 @@ class Player {
     skill = 0;
   }
 
-  double convertToMinutes(String dhm) {
-    return 0; //fill in later
+  //precondition: dhm is in the format "Days: Hours: Minutes" with no other characters
+  int convertToMinutes(String dhm) {
+    List<String> list = dhm.split(":");
+    List<int> intList;
+    for (int i = 0; i < list.length; i++) {
+      //converts each string (d, h, or m) to an int, then adds that int as an element to the int list
+      intList.add(int.parse(list[i]));
+    }
+    //converts days and hours to minutes, then sums with minutes to get the total minutes
+    return (intList[0] * 24 * 60 + intList[1] * 60 + intList[2]);
+    //source for split: https://www.tutorialkart.com/dart/dart-split-string/#:~:text=Dart%20Split%20String&text=You%20can%20split%20a%20string,split()%20method.&text=The%20function%20returns%20a%20list%20of%20strings
+    //source for int.parse: https://bezkoder.com/dart-parse-string-to-double-int/
   }
 
   String get getName {
