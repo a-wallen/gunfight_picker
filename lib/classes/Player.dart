dad2b0e847758ca60bbd825dc2d58dcf638a1258
David-S-2002 <dstanko@seattleu.edu>
2020-10-03 14:01:38 -0700

    Update Player.dart

--- lib/classes/Player.dart
+++ lib/classes/Player.dart
@@ -42,29 +42,40 @@ class Player {
   }
 
   void set player_name(String name) {
-    if (isAlpha(name)) this.name = name;
+    /*if (isAlpha(name))*/ this.name = name;
   }
 
   void set player_kdratio(String kd) {
-    if (isFloat(kd)) this.kdratio = double.parse(kd);
+    /*if (isFloat(kd))*/ this.kdratio = double.parse(kd);
   }
 
   void set player_hours(String hours) {
-    if (isDate(hours)) this.hoursPlayed = 0;
+    /*if (isDate(hours))*/ this.hoursPlayed = int.parse(hours);
   }
 
+<<<<<<< HEAD
   void printPl() {
     print("Player hours: $getName");
     print("Player name: $getKD");
     print("Player kd: $getSkill");
+=======
+  void printPl(){
+    print("Player name: $getName");
+    print("Player kd: $getKD");
+    print("Player kd: $getHours");
+>>>>>>> b4c0c2f6e81e54b715bfc95fde00933de3d34da2
   }
 
   String get getName {
     return name;
   }
 
-  String get getKD {
-    return name;
+  double get getKD {
+    return kdratio;
+  }
+
+  int get getHours {
+    return hoursPlayed;
   }
 
   int get getSkill {
