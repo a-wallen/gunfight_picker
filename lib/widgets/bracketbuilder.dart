93b5385575b8c7713aa81cfaa3ac45bfd34892be
varelandrew <61606497+varelandrew@users.noreply.github.com>
2020-10-09 22:07:20 -0700

    Bracket Builder

--- lib/widgets/bracketbuilder.dart
+++ lib/widgets/bracketbuilder.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:gunfight_picker/classes/Team.dart';
 
 class BracketBuilder extends StatefulWidget {
+  List<Widget> levels = [];
   List<Team> _teams;
   BracketBuilder(List<Team> tl) {
     this._teams = tl;
@@ -16,9 +17,54 @@ class _BracketBuilderState extends State<BracketBuilder> {
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height / 3.5,
-      child: Text(
-        "Hector's Widget",
-        textAlign: TextAlign.center,
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Card(
+                  child: ListTile(
+                    title: Text("Team 1"),
+                    subtitle: Text(""),
+                    onTap: ,
+                  ),
+                ),
+                Card(),
+                Card(),
+                Card(),
+                Card(),
+                Card(),
+                Card(),
+                Card(),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Card(),
+                Card(),
+                Card(),
+                Card(),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Card(),
+                Card(),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Card(),
+              ],
+            ),
+          ),
+        ], 
       ),
     );
   }
